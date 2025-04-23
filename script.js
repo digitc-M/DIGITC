5a1fa74a2f83847566fff879eedc29e7efccff6c
digitc-M <maketdigitc@gmail.com>
2025-04-23 16:48:18 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 
-console.log("Bienvenue sur DIGITC!");
+function acheter(produit) {
+    window.location.href = "https://wa.me/2250710037390?text=Bonjour%2C%20je%20suis%20int%C3%A9ress%C3%A9%20par%20le%20produit%20: " + encodeURIComponent(produit);
+}
