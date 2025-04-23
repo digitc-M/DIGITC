f28410372bc88dd37a5b1cb4f41e6415367b5c6e
digitc-M <maketdigitc@gmail.com>
2025-04-23 17:59:30 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
-function acheter(produit) {
-    window.location.href = "https://wa.me/2250710037390?text=Bonjour%2C%20je%20suis%20int%C3%A9ress%C3%A9%20par%20le%20produit%20: " + encodeURIComponent(produit);
-}
+document.querySelectorAll("button").forEach(button => {
+    button.onclick = () => alert("Fonction d'achat à venir !");
+};
